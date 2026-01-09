1423184c84beb1bb067be4090bb2619e02768b1e
Trishita <trishita@winadeal.com>
2026-01-09 19:49:15 +0530

    fix: add custom type definitions for multer to resolve build error

---
+++ backend/src/types.d.ts
@@ -0,0 +1,19 @@
+declare namespace Express {
+    export interface Request {
+        user?: any;
+        files?: any;
+    }
+    export namespace Multer {
+        export interface File {
+            fieldname: string;
+            originalname: string;
+            encoding: string;
+            mimetype: string;
+            size: number;
+            destination: string;
+            filename: string;
+            path: string;
+            buffer: Buffer;
+        }
+    }
+}
